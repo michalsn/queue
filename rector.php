7cccd8e22130d977023f001de5cc7e25f751494f
Michal Sniatala <michal@sniatala.pl>
2024-09-18 15:45:54 +0200

    Update to PHPUnit 10 (#46)
    * update to phpunit 10
    
    * fix phpunit workflow
    
    * update deptrac workflow
    
    * rector workflow update

--- rector.php
+++ rector.php
@@ -27,7 +27,6 @@ use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
 use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php73\Rector\FuncCall\StringifyStrNeedlesRector;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
@@ -82,7 +81,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         __DIR__ . '/app/Views',
 
-        JsonThrowOnErrorRector::class,
         StringifyStrNeedlesRector::class,
         YieldDataProviderRector::class,
 
